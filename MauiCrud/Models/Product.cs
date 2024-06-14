01d41306b956e1748ba649ebf19933cfa7c6426c
stenpatrick <158278859+stenpatrick@users.noreply.github.com>
2024-06-14 10:42:00 +0100

    tehtud

--- MauiCrud/Models/Product.cs
+++ MauiCrud/Models/Product.cs
@@ -1,9 +1,4 @@
 ﻿using SQLite;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MauiCrud.Models
 {
@@ -13,21 +8,5 @@ namespace MauiCrud.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-
-        public Product Clone() => MemberwiseClone() as Product;
-
-        public (bool IsValid, string? ErrorMessage) Validate()
-        {
-            if (string.IsNullOrWhiteSpace(Name)) 
-            {
-                return (false, $"{nameof(Name)} is required.");
-            }
-            else if(Price <= 0 ) 
-            {
-                return (false, $"{nameof(Price)} should be greater than 0.");
-            }
-            return (true, null);
-        }
-
     }
 }
